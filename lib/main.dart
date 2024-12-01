cede7c4855b1f58ed66e1e4cdb0d65ed52a421ae
Y-Joe177 <135008963+Y-Joe177@users.noreply.github.com>
2024-12-01 12:24:02 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:poultry_guard/screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(KIsWeb){ 
+  await firebase.initializeApp (
+    options: firebase0options(  
+      apiKey: "AIzaSyChHH3RopmaruhQlbHwMVVI-mStgzVKxDQ",
+      authDomain: "capstone-524b6.firebaseapp.com",
+      databaseURL: "https://capstone-524b6-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "capstone-524b6",
+      storageBucket: "capstone-524b6.firebasestorage.app",
+      messagingSenderId: "113151719878",
+      appId: "1:113151719878:web:8ed36f6a8b595a2b6b9ac7",
+      measurementId: "G-PDETR4EP19"));  
+  }else{
+    await firebase.initializeApp();
+  }
+  runApp(const MyApp());}
+  
 }
 
 class MyApp extends StatelessWidget {
